89ac6aec86f4536ddf0c2fa833cc788e6e8a6f18
Cesar <caospinamu@unal.edu.co>
2023-06-27 13:44:37 -0500

    ASignacion correcta, solo falta politica reemplazo

--- final/traduccion.c
+++ final/traduccion.c
@@ -7,12 +7,15 @@
 #define PAGE_SIZE 4096
 #define TLB_SIZE 16
 
-int numberUses;
+
+
+int usedSlot = 0;
 
 struct TBL_struct {
     int physical_address;
     int page_number;
     int displacement;
+    int numberUses;
     bool valid;
 };
 
@@ -35,13 +38,11 @@ struct TlbResult tlb_verify(struct TBL_struct* tbl, int physical_address) {
     struct TlbResult result = { false, 0, 0 };
 
     for (int i = 0; i < TLB_SIZE; i++) {
-        // printf("Las direcciones son: %d", tbl[i].physical_address);
-        // printf("Las pagina es son: %d", tbl[i].page_number);
-        // printf("El desplaza son: %d\n ", tbl[i].displacement);
         if (tbl[i].valid && tbl[i].physical_address == physical_address) {
             result.hit = true;
             result.page_number = tbl[i].page_number;
             result.displacement = tbl[i].displacement;
+            tbl[i].numberUses += 1;
             break;
         }
     }
@@ -58,6 +59,7 @@ struct TlbResult tlb_translateAddress(struct TBL_struct* tbl, int physical_addre
             tbl[i].physical_address = physical_address;
             tbl[i].page_number = page_number;
             tbl[i].displacement = displacement;
+            tbl[i].numberUses = 1;
             tbl[i].valid = true;
             break;
         }
@@ -66,6 +68,31 @@ struct TlbResult tlb_translateAddress(struct TBL_struct* tbl, int physical_addre
     struct TlbResult result = { false, page_number, displacement };
     return result;
 }
+
+
+// struct TlbResult tlb_update(unsigned int page_number, unsigned int frame_number) {
+//     // Replace the lastest input 
+//     int oldest_entry_index = 0;
+//     time_t oldest_entry_time = tlb[0].valid ? tlb[0].frame_number : 0;
+
+//     for (int i = 1; i < TLB_SIZE; i++) {
+//         if (!tlb[i].valid) {
+//             oldest_entry_index = i;
+//             break;
+//         }
+//         if (tlb[i].frame_number < oldest_entry_time) {
+//             oldest_entry_index = i;
+//             oldest_entry_time = tlb[i].frame_number;
+//         }
+//     }
+
+//     // Actualizar la entrada del TLB
+//     tlb[oldest_entry_index].valid = true;
+//     tlb[oldest_entry_index].page_number = page_number;
+//     tlb[oldest_entry_index].frame_number = frame_number;
+// }
+
+
 
 int main() {
     int virtual_address;
